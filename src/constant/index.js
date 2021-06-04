a596bd46fec03e42767aca51c7611966385f846b
robin-hassan <robiul.hassan12102@gmail.com>
2021-06-04 21:14:42 +0800

    added env

--- src/constant/index.js
+++ src/constant/index.js
@@ -1,3 +1,3 @@
-export const API_HOST = process.env.API_HOST || 'http://localhost:4000'
+export const API_HOST = process.env.REACT_APP_API_HOST || 'http://localhost:4000'
 
-export const WS_HOST = process.env.WS_HOST || 'ws://localhost:4000/cable'
+export const WS_HOST = process.env.REACT_APP_WS_HOST || 'ws://localhost:4000/cable'
